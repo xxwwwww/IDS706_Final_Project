3ab6c3027cb5f32d2efb91e6ada1fb35a092d1a6
Chang C <48408450+hohohokokoko@users.noreply.github.com>
2022-12-13 02:32:13 +0000

    Updated db query lib and microservice

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+"""FastAPI microservice"""
 from fastapi import FastAPI
 import uvicorn
 from dblib.dbquery import DB
@@ -9,23 +10,26 @@ db = DB()
 
 @app.get("/")
 async def root():
-    return {"message": "Hello There!"}
+    """Welcome message on homepage"""
+    return "Welcome to our movie database!"
 
 
-@app.get("/add/{num1}/{num2}")
-async def add(num1: int, num2: int):
-    """Add two numbers together"""
-
-    total = num1 + num2
-    return {"total": total}
+@app.get("/director/{director_name}")
+async def get_by_director(director_name: str):
+    """Return all movies directed by a specified director"""
+    return db.query1(director_name)
 
 
-@app.get("/query")
-async def query():
-    """Execute a SQL query"""
+@app.get("/goodrating")
+async def get_by_rating():
+    """Return all movies that have ratings higher than 9"""
+    return db.query2()
 
-    result = db.query1()
-    return {"result": result}
+
+@app.get("/longest")
+async def get_by_runtime():
+    """Return the top 10 longest movies"""
+    return db.query3()
 
 
 if __name__ == "__main__":
--- dblib/dbquery.py
+++ dblib/dbquery.py
@@ -1,8 +1,11 @@
-import MySQLdb
+"""Database query lib"""
 import os
+import json
+import MySQLdb
 
 
 class DB:
+    """DB utility class to connect and query"""
     def __init__(self):
         # get connected with the db
         self.connection = MySQLdb.connect(
@@ -14,21 +17,66 @@ class DB:
         # get the cursor of the connection
         self.cursor = self.connection.cursor()
 
-    # the followings are three queries
-    '''
-    query1: return all the names of movies directed by a specified director
-    '''
-    def query1(self, director_name):
-        sql1 = 'SELECT TITLE FROM MOVIE WHERE DIRECTOR = "' + director_name + '"'
-    
-    '''
-    query2: return the names of movies that have ratings higher than 9
-    '''
-    def query2(self):
-        sql2 = 'SELECT TITLE FROM MOVIE WHERE RATING > 9'
 
-    '''
-    query3: return the top 10 highest grossing movies
-    '''
+    def query1(self, director_name):
+        '''
+        query1: return all movies directed by a specified director
+        '''
+        sql1 = 'SELECT title, year, director\
+                FROM movie\
+                WHERE director = "' + director_name + '"'
+        self.cursor.execute(sql1)
+        data = self.cursor.fetchall()
+
+        result = []
+        for row in data:
+            movie = {}
+            movie['title'] = row[0]
+            movie['year'] = row[1]
+            movie['director'] = row[2]
+            result.append(movie)
+
+        return json.dumps(result)
+
+
+    def query2(self):
+        '''
+        query2: return all movies that have ratings higher than 9
+        '''
+        sql2 = 'SELECT title, year, rating\
+                FROM movie\
+                WHERE rating >= 9'
+        self.cursor.execute(sql2)
+        data = self.cursor.fetchall()
+
+        result = []
+        for row in data:
+            movie = {}
+            movie['title'] = row[0]
+            movie['year'] = row[1]
+            movie['rating'] = str(row[2])
+            result.append(movie)
+
+        return json.dumps(result)
+
+
     def query3(self):
-        sql3 = 'SELECT TITLE FROM MOVIE ORDER BY RATING DESC LIMIT 10'
+        '''
+        query3: return the top 10 longest movies
+        '''
+        sql3 = 'SELECT title, year, runtime\
+                FROM movie\
+                ORDER BY runtime DESC\
+                LIMIT 10'       
+        self.cursor.execute(sql3)
+        data = self.cursor.fetchall()
+
+        result = []
+        for row in data:
+            movie = {}
+            movie['title'] = row[0]
+            movie['year'] = row[1]
+            movie['runtime'] = row[2]
+            result.append(movie)
+
+        return json.dumps(result)
